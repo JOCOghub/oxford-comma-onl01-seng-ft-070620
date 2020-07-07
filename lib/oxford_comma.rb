8376cde89566cf8ec76304c4c9fe09b6f82e8e9c
Joseph cohen <jojocohen57@gmail.com>
2020-07-07 21:25:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -15,8 +15,7 @@ def oxford_comma(array)
     array.each.with_index do |fruit, idx|
       if fruit == "and" 
         new_str += fruit + " "
-      end
-      if idx == array.length - 1
+      elsif idx == array.length - 1
         new_str += fruit
       else
         new_str += fruit + ", "
