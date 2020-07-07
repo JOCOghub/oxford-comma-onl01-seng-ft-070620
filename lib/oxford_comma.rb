b1615d43b52ebde7e7c18a8e40c3474222172d2b
Joseph cohen <jojocohen57@gmail.com>
2020-07-07 20:30:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
-  if array.length > 2 
+  if array.length == 1 
+    array.join("")
+  end  if array.length > 2 
      array.insert(-2,"and")
      array.join(" ")
   end 
   
-  if array.length == 1 
-    array.join("")
-  end  
+  
   
   if array.length 
 end
