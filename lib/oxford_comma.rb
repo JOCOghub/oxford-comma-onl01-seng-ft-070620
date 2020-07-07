655f46189c142442a14be4cdb4de5d19e9d30882
Joseph cohen <jojocohen57@gmail.com>
2020-07-07 21:03:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,8 +10,9 @@ def oxford_comma(array)
     
   if array.length > 2 
     array.each.with_index do |name, idx|
-      if idx != (array.length - 1) && (name != "and")
-        name += ","
+      if idx != (array.length - 1) 
+        if name != "and"
+          name += ","
       end 
     end   
     array.insert(-2,"and")
