7260908f921d26d980c5cb278b32f02744312c25
Joseph cohen <jojocohen57@gmail.com>
2020-07-07 21:17:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,14 +8,17 @@ def oxford_comma(array)
     return array.join(" ")
   end
     
-  if array.length > 2 
+  if array.length > 2
+    new_arr = []
     array.join(", ")
     array.insert(-2,"and")
     array.each.with_index do |fruit, idx|
       if fruit != "and" || idx != array.length - 1
-        fruit += ","
+        new_arr << fruit += ","
+      else
+        new_arr << fruit
       end
     end
-    return array.join(',')
+    return array.join(' ')
   end
 end
